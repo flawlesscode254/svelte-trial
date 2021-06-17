26f8679aac27066159f7eb977cf07ab4d9d3036f
flawlesscode254 <denzme414@gmail.com>
2021-06-17 09:01:10 +0300

    trial

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Users from "../src/Users.svelte";
+</script>
+
+<main>
+  <Users 	
+  	name="Duncan"
+	post="What is up there young man. Why are you so annoying"
+	number="19" />
+  <Users
+  	name="James" 
+	post="What is up there young man. Why are you so annoying"
+	number="21" />
+  <Users 
+  	name="Allan"
+	post="What is up there young man. Why are you so annoying"
+	number="16" />
+  <Users 
+	name="Willem" 
+	post="What is up there young man. Why are you so annoying"
+	number="30" />
+</main>
+
+<style>
+</style>
---
+++ src/Users.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    export let name
+    export let number
+    export let post
+
+</script>
+
+<main class="main-file">
+    <h1>{name}</h1>
+    <h3>{post}</h3>
+    <p>{number}</p>
+</main>
+
+<style>
+
+</style>
